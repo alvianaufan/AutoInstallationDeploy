f33fff5322c4204ed7e51776bf4b9fd650415806
alvianaufan <alvianaufan@gmail.com>
2025-08-22 16:49:54 +0700

    Update install-php8.1.sh

--- install-php8.1.sh
+++ install-php8.1.sh
@@ -38,10 +38,3 @@ sudo apt-get install -y \
 echo "=== PHP 8.1 installation completed successfully ==="
 php -m
 php -v
-#install-compser
-curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-HASH=`curl -sS https://composer.github.io/installer.sig`
-echo $HASH
-php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
-composer
